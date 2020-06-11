be4826d580fab5a1c8955df9d35612637c0478ed
Sujata Patil <sujatapatil2010@gmail.com>
2020-06-10 19:55:12 -0500

    09 Added eureka client in all 3 microservices

--- movie-data-service/rating-data-service/src/main/java/com/suziesta/moviedataservice/MovieDataServiceApplication.java
+++ movie-data-service/rating-data-service/src/main/java/com/suziesta/moviedataservice/MovieDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.suziesta.moviedataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieDataServiceApplication {
 
 	public static void main(String[] args) {
